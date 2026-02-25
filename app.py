51612dca2b24a9c59500c4c26f1a7fc93d56aaca
Suraj192 <surajsharma1921@gmail.com>
2026-02-25 20:31:32 +0200

    prediction of fradulent with two variable rendered to predict html successfully

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import sqlite3
+import joblib
+import numpy as np
+
+
 
 app = Flask(__name__)
+
+model = joblib.load("models/fraud_model.pkl")
 
 def get_fraud_stats():
     conn = sqlite3.connect("fraud.db")
@@ -35,7 +41,33 @@ def index():
     return render_template("index.html", normal = stats["normal"],
     fraud= stats["fraud"])
 
+
+@app.route("/predict", methods=["GET", "POST"])
+def predict():
+    prediction = None
+
+    if request.method == "POST":
+        time = float(request.form["Time"])
+        amount = float(request.form["Amount"]) # getting input value from form
+
+        input_data = [time] + [0]*28 + [amount] # out of 30 feature v1 - v28 feature is set to be 0 for input array
+
+        input_array = np.array(input_data).reshape(1,-1)
+
+        result = model.predict(input_array)[0]
+
+        if result == 1:
+            prediction = " ⚠️ Fradulent Transaction"
+        else:
+            prediction = " ✅ Normal Transaction"
+
+    return render_template("predict.html", prediction = prediction)
+
+
+
+
 if __name__ == "__main__":
+    print(app.url_map)
     app.run(debug=True)
 
 
